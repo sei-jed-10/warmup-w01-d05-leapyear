9357e66c8c7d1e67e39478dd8421b2a87990981c
daghustani <57670752+daghustani@users.noreply.github.com>
2019-11-22 16:43:53 +0300

    Add files via upload

---
+++ warmup-leapyear-solution.js
@@ -0,0 +1,16 @@
+const isLeapYear = function (year) {
+  if (year % 4 === 0 && year % 100 !== 0) { return true; }
+  else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+      return true;
+  }
+  else return false;
+}
+
+//// another solution
+
+const isLeapYear2 = (year) => ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+console.log(isLeapYear(1990))
+console.log(isLeapYear(1996))
+console.log(isLeapYear2(1997))
+console.log(isLeapYear2(2000))
